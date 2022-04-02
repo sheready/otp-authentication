62979e5714a4648ca4112170156b30b916435005
sheready <csstlw@gmail.com>
2022-04-02 12:47:23 +0300

    add verify otp code function

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
-from users.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from users.forms import UserCreationForm, VerifyForm
+from . import verify
 # Create your views here.
 
+@login_required
 def index(request):
     return render(request, 'index.html')
 
@@ -10,7 +13,22 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('index')
+            verify.send(form.cleaned_data.get('phone'))
+            return redirect('users:index')
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form':form})
+
+@login_required
+def verify_code(request):
+    if request.method == 'POST':
+        form = VerifyForm(request.POST)
+        if form.is_valid():
+            code = form.cleaned_data.get('code')
+            if verify.check(request.user.phone, code):
+                request.user.is_verified = True
+                request.user.save()
+                return redirect('users:index')
+    else:
+        form = VerifyForm()
+    return render(request, 'verify.html', {'form': form})
