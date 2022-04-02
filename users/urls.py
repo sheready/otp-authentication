6367db7af607259ebd75adc913b5b34cafb85699
sheready <csstlw@gmail.com>
2022-04-02 12:46:22 +0300

    add verify url

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
-
+app_name = 'users'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('register/', views.register),
+    path('register/', views.register, name="register"),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('verify/', views.verify_code, name='verify'),
 ]
